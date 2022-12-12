e996a2f25bd73a6dd1dc36b9f8212c6813d6d0eb
hachem-bouhamidi <Ha.bouhamidi@aui.ma>
2022-12-12 20:01:07 +0100

    changed guests love into Meet our RA's

--- Frontend/src/components/featuredProperties/FeaturedProperties.jsx
+++ Frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,17 +1,21 @@
 import "./featuredProperties.css";
+import Ra_1 from "../../Ra Images/282573183_1448410478947047_5918019139437515482_n.jpg"
+import Ra_2 from "../../Ra Images/276220823_4885430671552464_4338294145903081726_n.jpg"
+import Ra_3 from "../../Ra Images/318111492_3022825464676656_349394368157766881_n.jpg"
+import Ra_4 from "../../Ra Images/312093583_1989694124563014_5921161818930669127_n.jpg"
 
 const FeaturedProperties = () => {
   return (
     <div className="fp">
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          src={Ra_1}
           alt=""
           className="fpImg"
         />
-        <span className="fpName">Aparthotel Stare Miasto</span>
-        <span className="fpCity">Madrid</span>
-        <span className="fpPrice">Starting from $120</span>
+        <span className="fpName">Nour Maski</span>
+        <span className="fpCity">SSE student</span>
+        <span className="fpPrice">Building 36</span>
         <div className="fpRating">
           <button>8.9</button>
           <span>Excellent</span>
@@ -19,13 +23,13 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
+          src={Ra_2}
           alt=""
           className="fpImg"
         />
-        <span className="fpName">Comfort Suites Airport</span>
-        <span className="fpCity">Austin</span>
-        <span className="fpPrice">Starting from $140</span>
+        <span className="fpName">Younes Boubbou</span>
+        <span className="fpCity">SSE student</span>
+        <span className="fpPrice">Building 18</span>
         <div className="fpRating">
           <button>9.3</button>
           <span>Exceptional</span>
@@ -33,13 +37,13 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/232902339.jpg?k=3947def526b8af0429568b44f9716e79667d640842c48de5e66fd2a8b776accd&o=&hp=1"
+          src={Ra_3}
           alt=""
           className="fpImg"
         />
-        <span className="fpName">Four Seasons Hotel</span>
-        <span className="fpCity">Lisbon</span>
-        <span className="fpPrice">Starting from $99</span>
+        <span className="fpName">Ayoub Mabkhout</span>
+        <span className="fpCity">SSE student</span>
+        <span className="fpPrice">Building 21</span>
         <div className="fpRating">
           <button>8.8</button>
           <span>Excellent</span>
@@ -47,13 +51,13 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
+          src={Ra_4}
           alt=""
           className="fpImg"
         />
-        <span className="fpName">Hilton Garden Inn</span>
-        <span className="fpCity">Berlin</span>
-        <span className="fpPrice">Starting from $105</span>
+        <span className="fpName">Amine Ahenjir</span>
+        <span className="fpCity">SHSS student</span>
+        <span className="fpPrice">Farah Inn</span>
         <div className="fpRating">
           <button>8.9</button>
           <span>Excellent</span>
